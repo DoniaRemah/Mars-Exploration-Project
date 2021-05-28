756bf4b9385e844ebcfdbe115fc0ba5b601ba53f
Ola Ahmed <abouelhadid.ola@gmail.com>
2021-05-28 17:16:46 +0200

    Rovers

--- Rovers/Rovers.cpp
+++ Rovers/Rovers.cpp
@@ -26,6 +26,42 @@ Mission* Rover::GetMission()
 	return m_mission;
 }
 
+void Rover::ResetMissions() 
+{
+	Num_Of_ExecMissions = 0;
+}
+
+void Rover::IncrementMissions() 
+{
+	Num_Of_ExecMissions++;
+}
+
+void Rover::SetInCheckUp(bool status) 
+{
+	InCheckUp = status;
+}
+
+void Rover::SetIsAvailable(bool status)
+{
+	IsAvailable = status;
+}
+
+bool Rover:: GetAvailable()
+{
+	return IsAvailable;
+}
+
+bool Rover::GetInCheckup() 
+{
+	return InCheckUp;
+}
+
+int Rover::GetNumOfMissions() 
+{
+	return Num_Of_ExecMissions;
+}
+
+
 
 
 Rover::~Rover() // Destructor
